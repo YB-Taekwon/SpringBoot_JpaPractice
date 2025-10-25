858878bf2b65be5024f1820f44e38c87b3ad0132
XXN <jaechan27.0.2.28@gmail.com>
2025-10-26 00:49:24 +0900

    feat: Order / OrderProduct - 비즈니스 로직 추가

--- src/main/java/com/ian/jpapractice/domain/Order.java
+++ src/main/java/com/ian/jpapractice/domain/Order.java
@@ -8,6 +8,10 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ian.jpapractice.domain.DeliveryStatus.COMP;
+import static com.ian.jpapractice.domain.OrderStatus.CANCEL;
+import static com.ian.jpapractice.domain.OrderStatus.ORDER;
+
 @Entity
 @Table(name = "orders")
 @Getter
@@ -24,7 +28,7 @@ public class Order {
     private User user;
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
-    private List<OrderProduct> orderProductList = new ArrayList<>();
+    private List<OrderProduct> orderProducts = new ArrayList<>();
 
     private LocalDateTime orderDate;
 
@@ -40,13 +44,55 @@ public class Order {
         user.getOrders().add(this);
     }
 
+    /**
+     * 양방향 연결 메서드
+     */
+    // Order <-> OrderProduct 양뱡향 연결
     public void addOrderProduct(OrderProduct orderProduct) {
-        orderProductList.add(orderProduct);
+        orderProducts.add(orderProduct);
         orderProduct.setOrder(this);
     }
 
+    // Order <-> Delivery 양방향 연결
     public void addDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
+    }
+
+    /**
+     * 주문 생성 팩토리 메서드
+     */
+    public static Order createOrder(User user, Delivery delivery, OrderProduct... orderProducts) {
+        Order order = new Order();
+        order.setUser(user);
+        order.setDelivery(delivery);
+        for (OrderProduct orderProduct : orderProducts) {
+            order.addOrderProduct(orderProduct);
+        }
+        order.setStatus(ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    /**
+     * 비즈니스 로직
+     */
+    // 주문 취소
+    public void cancelOrder() {
+        if (delivery.getStatus() == COMP) {
+            throw new IllegalStateException("이미 배송 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(CANCEL);
+        for (OrderProduct orderProduct : orderProducts) {
+            orderProduct.cancel();
+        }
+    }
+
+    // 총 주문 금액 조회
+    public int getTotalPrice() {
+        return orderProducts.stream()
+                .mapToInt(OrderProduct::getTotalPrice)
+                .sum();
     }
 }
--- src/main/java/com/ian/jpapractice/domain/OrderProduct.java
+++ src/main/java/com/ian/jpapractice/domain/OrderProduct.java
@@ -26,4 +26,31 @@ public class OrderProduct {
 
     private int orderPrice;
     private int count;
+
+    /**
+     * 주문 상품 생성 팩토리 메서드
+     */
+    public static OrderProduct createOrderProduct(Product product, int orderPrice, int count) {
+        OrderProduct orderProduct = new OrderProduct();
+        orderProduct.setProduct(product);
+        orderProduct.setOrderPrice(orderPrice);
+        orderProduct.setCount(count);
+
+        product.removeStock(count);
+
+        return orderProduct;
+    }
+
+    /**
+     * 비즈니스 로직
+     */
+    // 재고 수량 복구
+    public void cancel() {
+        getProduct().addStock(count);
+    }
+
+    // 총 주문 금액 조회
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
