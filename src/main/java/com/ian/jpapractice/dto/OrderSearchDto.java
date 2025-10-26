1521bbedcd0027f209922df04d4045bc2e7d2f58
ian <loundo001@gmail.com>
2025-10-26 23:20:32 +0900

    feat: 주문 검색 기능 추가
    dsl 동적 쿼리 - 필터링, 페이징, 검색 기능 구현

--- src/main/java/com/ian/jpapractice/dto/OrderSearchDto.java
+++ src/main/java/com/ian/jpapractice/dto/OrderSearchDto.java
@@ -1,4 +1,12 @@
 package com.ian.jpapractice.dto;
 
+import com.ian.jpapractice.domain.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class OrderSearchDto {
+    private String userName;
+    private OrderStatus orderStatus;
 }
--- src/main/java/com/ian/jpapractice/repository/OrderRepository.java
+++ src/main/java/com/ian/jpapractice/repository/OrderRepository.java
@@ -1,15 +1,27 @@
 package com.ian.jpapractice.repository;
 
 import com.ian.jpapractice.domain.Order;
+import com.ian.jpapractice.domain.OrderStatus;
+import com.ian.jpapractice.domain.QOrder;
+import com.ian.jpapractice.domain.QUser;
+import com.ian.jpapractice.dto.OrderSearchDto;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class OrderRepository {
 
+    private final JPAQueryFactory query;
     private final EntityManager em;
+    private static final QOrder order = QOrder.order;
+    private static final QUser user = QUser.user;
 
     public void save(Order order) {
         em.persist(order);
@@ -17,5 +29,28 @@ public class OrderRepository {
 
     public Order findById(Long id) {
         return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearchDto cond) {
+        return query
+                .select(order)
+                .from(order)
+                .join(order.user, user)
+                .where(
+                        statusEq(cond.getOrderStatus()),
+                        nameLike(cond.getUserName())
+                )
+                .limit(1000)
+                .fetch();
+    }
+
+    private BooleanExpression statusEq(OrderStatus statusCond) {
+        return statusCond == null ? null : order.status.eq(statusCond);
+    }
+
+    private BooleanExpression nameLike(String nameCond) {
+        if (!StringUtils.hasText(nameCond)) return null;
+
+        return user.name.like("%" + nameCond + "%");
     }
 }
